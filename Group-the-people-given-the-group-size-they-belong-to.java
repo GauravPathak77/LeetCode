0e83e0b8721c94de2b54eef7d6916eb443e325fd
RockingCoder <115170827+GauravPathak77@users.noreply.github.com>
2023-09-11 11:13:11 +0530

    11-09-2023

---
+++ Group-the-people-given-the-group-size-they-belong-to.java
@@ -0,0 +1,18 @@
+class Group-the-people-given-the-group-size-they-belong-to {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<groupSizes.length; i++){
+            if(!map.containsKey(groupSizes[i])){
+            map.put(groupSizes[i], new ArrayList<>());
+
+            }
+            map.get(groupSizes[i]).add(i);
+            if(map.get(groupSizes[i]).size() == groupSizes[i]){
+                list.add(new ArrayList<>(map.get(groupSizes[i])));
+                map.get(groupSizes[i]).clear();
+            }
+        }
+        return list;
+    }
+}
