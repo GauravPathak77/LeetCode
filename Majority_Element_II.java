c57bfaad83ff054ca7a9919c7ad8e9283e2d31d0
RockingCoder <115170827+GauravPathak77@users.noreply.github.com>
2023-10-05 22:45:05 +0530

    Oct 05, 2023
    Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
    
    Example 1:
    
    Input: nums = [3,2,3]
    Output: [3]
    Example 2:
    
    Input: nums = [1]
    Output: [1]
    Example 3:
    
    Input: nums = [1,2]
    Output: [1,2]
     
    Constraints:
    
    1 <= nums.length <= 5 * 104
    -109 <= nums[i] <= 109

---
+++ Majority_Element_II.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+        
+        int n = nums.length;
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+        
+        return result;
+    }
+}
