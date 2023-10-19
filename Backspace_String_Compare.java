ef3407901e7a9f867a78b8d5eb613a2130dde763
RockingCoder <115170827+GauravPathak77@users.noreply.github.com>
2023-10-19 20:08:45 +0530

    Create Backspace_String_Compare.java
    Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
    
    Note that after backspacing an empty text, the text will continue empty.
    
    Example 1:
    
    Input: s = "ab#c", t = "ad#c"
    Output: true
    Explanation: Both s and t become "ac".
    Example 2:
    
    Input: s = "ab##", t = "c#d#"
    Output: true
    Explanation: Both s and t become "".
    Example 3:
    
    Input: s = "a#c", t = "b"
    Output: false
    Explanation: s becomes "c" while t becomes "b".
    
    Constraints:
    
    1 <= s.length, t.length <= 200
    s and t only contain lowercase letters and '#' characters.

---
+++ Backspace_String_Compare.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        StringBuilder s1 = new StringBuilder(s);
+        StringBuilder s2 = new StringBuilder(t);
+        for(int i=0; i < s1.length(); i++){
+            if(s1.charAt(i) == '#'){
+                if(i == 0){
+                    s1.deleteCharAt(i);
+                    i--;
+                    continue;
+                }
+                s1.deleteCharAt(i);
+                if(s1.length() == 1){
+                    s1.deleteCharAt(0);
+                    break;
+                }
+                s1.deleteCharAt(i-1);
+                i-=2;
+            }
+        }
+        
+        for(int i=0; i < s2.length(); i++){
+            if(s2.charAt(i) == '#'){
+                if(i == 0){
+                    s2.deleteCharAt(i);
+                    i--;
+                    continue;
+                }
+                s2.deleteCharAt(i);
+                if(s2.length() == 1){
+                    s2.deleteCharAt(0);
+                    break;
+                }
+                s2.deleteCharAt(i-1);
+                i-=2;
+            }
+        }
+        s = s1.toString();
+        t = s2.toString();
+        if(s.equals(t)){
+            return true;
+        }
+        return false;
+    }
+}
