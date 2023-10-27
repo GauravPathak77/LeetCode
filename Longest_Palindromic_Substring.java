2febf79c4f357e26a9917d8a2739c0b369a81806
RockingCoder <115170827+GauravPathak77@users.noreply.github.com>
2023-10-27 23:35:58 +0530

    Oct 27, 2023
    Given a string s, return the longest palindromic substring in s.
    
    Example 1:
    
    Input: s = "babad"
    Output: "bab"
    Explanation: "aba" is also a valid answer.
    Example 2:
    
    Input: s = "cbbd"
    Output: "bb"
     
    
    Constraints:
    
    1 <= s.length <= 1000
    s consist of only digits and English letters.

---
+++ Longest_Palindromic_Substring.java
@@ -0,0 +1,24 @@
+f (s.length() <= 1) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int start = 0;
+        int end = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, end + 1);
