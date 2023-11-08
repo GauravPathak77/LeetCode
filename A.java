482db12462fb2290a733af8dfd4fafeaa00854e2
RockingCoder <115170827+GauravPathak77@users.noreply.github.com>
2023-11-08 20:51:59 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,8 @@
+int xDist = Math.abs(sx - fx);
+        int yDist = Math.abs(sy - fy);
+
+        if (xDist == 0 && yDist == 0) {
+            return t != 1;
+        }
+
+        return xDist <= t && yDist <= t;
