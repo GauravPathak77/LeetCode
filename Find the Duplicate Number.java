2c38c4ccb6cde3ae5d727d397e19eb9aa7d63a25
RockingCoder <115170827+GauravPathak77@users.noreply.github.com>
2024-03-24 12:41:19 +0530

    March 24, 2024
    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
    
    There is only one repeated number in nums, return this repeated number.
    
    You must solve the problem without modifying the array nums and uses only constant extra space.
    
    Example 1:
    
    Input: nums = [1,3,4,2,2]
    Output: 2
    Example 2:
    
    Input: nums = [3,1,3,4,2]
    Output: 3
    Example 3:
    
    Input: nums = [3,3,3,3,3]
    Output: 3
     
    
    Constraints:
    
    1 <= n <= 105
    nums.length == n + 1
    1 <= nums[i] <= n
    All the integers in nums appear only once except for precisely one integer which appears two or more times.
     
    
    Follow up:
    
    How can we prove that at least one duplicate number must exist in nums?
    Can you solve the problem in linear runtime complexity?

---
+++ Find the Duplicate Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+    }
+}class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                ans = nums[i];
+                break;
+            }
+            map.put(nums[i], 0);
+        }
+        return ans;
+    }
+}
