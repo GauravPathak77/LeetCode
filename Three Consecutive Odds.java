9b79d0401d4d90fec08ff4f060592a48468cd0db
RockingCoder <115170827+GauravPathak77@users.noreply.github.com>
2024-07-01 10:04:27 +0530

    July 01, 2024
    Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
     
    
    Example 1:
    
    Input: arr = [2,6,4,1]
    Output: false
    Explanation: There are no three consecutive odds.
    Example 2:
    
    Input: arr = [1,2,34,3,4,5,7,23,12]
    Output: true
    Explanation: [5,7,23] are three consecutive odds.
     
    
    Constraints:
    
    1 <= arr.length <= 1000
    1 <= arr[i] <= 1000

---
+++ Three Consecutive Odds.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] % 2 != 0){
+                if((i+2 < arr.length) && (arr[i+1] % 2 != 0) && (arr[i+2] % 2 != 0)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
